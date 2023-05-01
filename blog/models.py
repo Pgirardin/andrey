8cc5e414fb7806f76762cfeb417eaad37bde8bd2
Pgirardin <ndayisengapgirard@gmail.com>
2023-05-02 00:49:32 +0300

    first commit

---
+++ blog/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+
+# import datetime
+
+# Create your models here.
+
+
+class Blog(models.Model):
+    title = models.CharField(max_length=100)
+    pub_date = models.DateTimeField()
+    body = models.TextField()
+    image = models.ImageField(upload_to='images/')
+
+    def __str__(self):
+        return self.title
